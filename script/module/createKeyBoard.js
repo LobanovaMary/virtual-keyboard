da9bacc32f098a893e77d2a5a719398d0c28c622
Lobanova Mary <lobanovama1@gmail.com>
2023-05-01 22:49:06 +0300

    feat: add function fill text area

--- script/module/createKeyBoard.js
+++ script/module/createKeyBoard.js
@@ -1,15 +1,13 @@
-import eventListener from './eventListener.js';
 import keys from '../keys.js';
 
 const createKeyBoard = () => {
   const keyboardField = document.querySelector('.keybord-container');
 
-  const htmlKeys = keys.map(key => { return `<div class='key ${key.code}'><span>${key.key}</span></div>`}).join(" ");
+  const htmlKeys = keys
+    .map((key) => `<div class='key ${key.class}' data-code='${key.code}'><span>${key.key}</span></div>`)
+    .join(' ');
 
-  keyboardField.insertAdjacentHTML('afterbegin',htmlKeys)
-
-  console.log('hi from keyboard');
-  console.log(keys)
-  eventListener();
+  keyboardField.insertAdjacentHTML('afterbegin', htmlKeys);
 };
+
 export default createKeyBoard;
